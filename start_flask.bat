0888c3dcfa805cfdf53b99abc8d7baaf72a4a970
Yaakov Monchaersh <122309967+coby98765@users.noreply.github.com>
2025-02-20 11:41:41 +0200

    Update start_flask.bat

--- start_flask.bat
+++ start_flask.bat
@@ -1,3 +1,11 @@
 @echo off
+echo Activating virtual environment...
 call .venv\Scripts\activate
+
+echo Installing dependencies from pyproject.toml...
+poetry install
+
+echo Starting Flask server...
 flask --app logger_backend run
+
+pause
